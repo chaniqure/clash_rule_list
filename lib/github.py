8f5dbbca6774c14f751646572edd4129448644a5
cc <1255861908@qq.com>
2022-04-18 00:24:45 +0800

    提交github gist相关操作

--- lib/github.py
+++ lib/github.py
@@ -2,37 +2,63 @@ import json
 
 import requests
 
+from util import logger
 
-def github():
-    api_url = "https://api.github.com"
-    api_token = 'your_token_goes_here'
+log = logger.get_logger('clash')
 
-    # form a request URL
-    url = api_url + "/gists"
-    print
-    "Request URL: %s" % url
 
-    # print headers,parameters,payload
-    headers = {'Authorization': 'token %s' % api_token}
-    params = {'scope': 'gist'}
-    payload = {"description": "GIST created by python code", "public": True, "files": {"python request module": {
-        "content": "Python requests has 3 parameters: 1)Request URL\n 2)Header Fields\n 3)Parameter \n4)Request body"}}}
+class GistFile:
+    def __init__(self, name, content):
+        self.name = name
+        self.content = content
 
-    # make a requests
-    res = requests.post(url, headers=headers, params=params, data=json.dumps(payload))
 
-    # print response --> JSON
-    print
-    res.status_code
-    print
-    res.url
-    print
-    res.text
-    j = json.loads(res.text)
+class GistCreateRequest:
+    def __init__(self, name: str, public: bool, file_list: list[GistFile]):
+        self.public = public
+        self.description = name
+        files = {}
+        for file in file_list:
+            files[file.name] = {
+                'content': file.content
+            }
+        self.files = files
 
-    # Print created GIST's details
-    for gist in range(len(j)):
-        print
-        "Gist URL : %s" % (j['url'])
-        print
-        "GIST ID: %s" % (j['id'])
+
+class Github:
+
+    def __init__(self, token):
+        self.__header = {'Authorization': 'token %s' % token}
+        self.__param = {'scope': 'gist'}
+        self.__api_url = "https://api.github.com/gists"
+
+    def gist_create(self, name, files):
+        payload = GistCreateRequest(files, False, name)
+        requests.packages.urllib3.disable_warnings()
+        return requests.post(self.__api_url,
+                             headers=self.__header,
+                             params=self.__param,
+                             data=json.dumps(obj=payload.__dict__, ensure_ascii=False),
+                             verify=False).ok
+
+    def gist_list(self):
+        requests.packages.urllib3.disable_warnings()
+        return requests.get(self.__api_url, headers=self.__header, params=self.__param, verify=False).text
+
+    def gist_update(self, name, files):
+        payload = GistCreateRequest(name, False, files)
+        return requests.patch('{}/{}'.format(self.__api_url, self.__get_gist_id(name)),
+                              headers=self.__header,
+                              params=self.__param, data=json.dumps(obj=payload.__dict__, ensure_ascii=False),
+                              verify=False).ok
+
+    def gist_delete(self, name):
+        return requests.delete('{}/{}'.format(self.__api_url, self.__get_gist_id(name)), headers=self.__header,
+                               params=self.__param, verify=False).ok
+
+    def __get_gist_id(self, name):
+        requests.packages.urllib3.disable_warnings()
+        gists = json.loads(self.gist_list())
+        for d in gists:
+            if d['description'] == name:
+                return d['id']
